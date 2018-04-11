dd9496306504a5ef5b7faed05d5c2f0b632435be
JxNull <35373708+JxNull@users.noreply.github.com>
2018-04-11 16:28:57 -0400

    Update and rename Python_0.py to Python_000_Strings.py

---
+++ Python_000_Strings.py
@@ -0,0 +1,24 @@
+# Python000
+#Author: Jingxiong Wu
+
+#April/11/2018   
+#Python exercise project 0
+
+# String exercise
+print("Hello World")
+
+# Multiple repeat strings
+print("Hello"*3)
+
+# single & double quote
+print("I don't know.")
+print('He said:"I don\'t know"')  # backslash makes whatever behind as part of string
+print('He said:\n"I don\'t know"') # \n use case
+print('c:\user\desktop\nothing') # backslash mass up case
+print(r'c:\user\desktop\nothing') # r before string, backslash no special meaning, file path,etc.
+
+#String can be variable
+name= 'Jx '
+print(name)
+print(name+'Wu\n')
+print((name+'Wu\n')*3)
