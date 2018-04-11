966f59c380f53194d21478cf17be3ff4eb8253f2
JxNull <35373708+JxNull@users.noreply.github.com>
2018-04-11 16:10:16 -0400

    Update and rename Hello_World.py to Python_0.py

--- Hello_World.py
+++
@@ -1,8 +0,0 @@
-# Python000
-Author: Jingxiong Wu
-
-April/11/2018   
-Python exercise project 0
-
-# "Hello World" exercise
-print("Hello World")
---
+++ Python_0.py
@@ -0,0 +1,21 @@
+# Python000
+#Author: Jingxiong Wu
+
+#April/11/2018   
+#Python exercise project 0
+
+# String exercise
+print("Hello World")
+
+# Multiple repeat strings
+print("Hello"*3)
+
+# single and double quote
+print("I don't know.")
+print('He said:"I don\'t know"')  # backslash makes whatever behind as part of string
+
+
+# Basic math
+# addation
+print(11+22)
+#
