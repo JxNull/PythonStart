527b6ee635d0ffb752fe4d6641205cef760c2cfb
JxNull <35373708+JxNull@users.noreply.github.com>
2018-04-11 16:31:49 -0400

    Update and rename README.py to README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# PythonStart
+Author: JxWu
+
+April/1st/2018   
+Python exercise project  in purpose of improve programming skill
+This Python Project begin with fundamental materials
+
--- README.py
+++
@@ -1,6 +0,0 @@
-# PythonStart
-Author: Jingxiong Wu
-
-April/11/2018   
-Python exercise project 
-This Python Project begin with fundamental materials in purpose of improve programming skill
