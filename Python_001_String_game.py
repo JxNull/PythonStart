7876f071bfc345d9728b3b5b8cff78892a514a1e
JxNull <35373708+JxNull@users.noreply.github.com>
2018-04-12 17:17:17 -0400

    Update Python_001_String_game.py

--- Python_001_String_game.py
+++ Python_001_String_game.py
@@ -1,9 +1,13 @@
+# Python001
+#Author: Jingxiong Wu
 
+#April/12/2018   
+#Python exercise project 1
 
 print ('-----------Python start------------')
 temp = input("Guess Jx faviourate number [1 2 3 4 5 6]:")
-guess = int(temp)
-if guess == 6 :
+guess = int(temp)  # indicates input type in integer 
+if guess == 6 :    # compare input interger with integer
     print('winner winner chicken dinner')
 else:
     print('The answer is 6')
