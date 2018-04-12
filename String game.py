1accc1d5884647c0296c4a474844811fbb319f3d
JxNull <35373708+JxNull@users.noreply.github.com>
2018-04-12 17:13:36 -0400

    Add files via upload

---
+++ String game.py
@@ -0,0 +1,10 @@
+print ('-----------Python start------------')
+temp = input("Guess Jx faviourate number [1 2 3 4 5 6]:")
+guess = int(temp)
+if guess == 6 :
+    print('winner winner chicken dinner')
+else:
+    print('The answer is 6')
+
+print('----------Game over-----------------')
+print('----------Python End----------------')
