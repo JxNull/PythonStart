71438d7a362b88d44570cf0acccb173a62364ef1
JxNull <35373708+JxNull@users.noreply.github.com>
2018-05-03 04:29:00 -0400

    Create Python_002_if_else.py

---
+++ Python_Baisc/Python_002_if_else.py
@@ -0,0 +1,23 @@
+# Python002
+#Author: Jingxiong Wu
+
+#April/30/2018   
+#Python exercise project 2
+#if_else statment
+
+name = 'JxNull'
+age = 24
+
+beerage=21
+
+if age<beerage:
+    print("You are not allow to buy beer. \n")
+else:
+    print("What would you like to drink? \n")
+    
+if name is "VN":
+    print("Hello VN.\n")
+elif name is "JxNul":
+    print("Hellp JxNull\n")
+else:
+    print("plese regsister a name in system")
