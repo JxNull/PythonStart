735217d1d5bf31ac79686a5de407d0d77cf9484d
JxNull <35373708+JxNull@users.noreply.github.com>
2018-05-07 04:58:19 -0400

    Create Python_011_variable.py

---
+++ Python_Baisc/Python_011_variable.py
@@ -0,0 +1,29 @@
+# Python011
+#Author: Jingxiong Wu
+
+#May/05/2018   
+#Python exercise project 11
+''' variable example'''
+
+a="I am global variable"
+
+def print_v_a():
+  print(a)
+
+def print_v_b(x):
+  print(x)
+
+print_v_a()       # result in  "I am global variable"
+print_v_b(a)      # result in "I am global variable"
+
+'''if variable write in the function ifself, then it becames a local variable'''
+
+def print_v_c():
+  a="I am a local variable"
+  print(a)
+
+def print_v_d():
+  print(a)
+ 
+print_v_c()      # result in "I am local variable"
+print_v_d(a)      # result in "I am global variable"
