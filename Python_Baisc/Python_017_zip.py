301ddc8c39d71970feca5fad97dbd9e9e6946eb5
JxNull <35373708+JxNull@users.noreply.github.com>
2018-05-08 05:54:45 -0400

    Create Python_017_zip.py

---
+++ Python_Baisc/Python_017_zip.py
@@ -0,0 +1,13 @@
+# Python017
+#Author: Jingxiong Wu
+
+#May/07/2018   
+#Python exercise project 17
+
+''' zip example'''
+
+category=['Name','Gender','Age','Job']
+deatils=['Jx','Male','24','EE']
+
+Jx=dict(zip(category,deatils))
+print(Jx)
