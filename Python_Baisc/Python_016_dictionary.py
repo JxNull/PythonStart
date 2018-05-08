1efdb99421b3c4040dede1bf8830ff3b3192f05f
JxNull <35373708+JxNull@users.noreply.github.com>
2018-05-08 05:47:50 -0400

    Create Python_016_dictionary.py

---
+++ Python_Baisc/Python_016_dictionary.py
@@ -0,0 +1,17 @@
+# Python016
+#Author: Jingxiong Wu
+
+#May/07/2018   
+#Python exercise project 16
+
+''' dictionary example'''
+dict_zero={'name':'vn', 'gender':'female','age':'23'}
+
+print(dict_zero)
+print('name')
+print('gender')
+print('age')
+
+for a,b in dict_zero.items():
+    print(a,' : ',b)
+    print(b,' ->  ',a)
