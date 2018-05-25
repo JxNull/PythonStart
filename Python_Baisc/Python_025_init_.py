247420d15b29e72a7c98eabe5b998b85d2c3b073
JxNull <35373708+JxNull@users.noreply.github.com>
2018-05-25 04:56:19 -0400

    Create Python_025_init_.py

---
+++ Python_Baisc/Python_025_init_.py
@@ -0,0 +1,33 @@
+# Python025
+#Author: Jingxiong Wu
+
+#May/12/2018   
+#Python exercise project 25
+
+''' __init__'''
+
+class Token:
+    supply=5
+    def __init__(self):
+        print("Token brought :"+str(self.supply))
+        
+
+    def buyticket(self,x):
+        print('used '+ str(x)+"token\n")
+        self.supply=self.supply-x
+
+    def checktoken(self):
+        if self.supply<=0:
+            print("You should buy more token.\n")
+        else:
+            print("Token avaliable:" + str(self.supply))
+
+
+person_a=Token()
+person_b=Token()
+
+person_a.buyticket(1)
+person_a.checktoken()
+person_b.buyticket(2)
+person_b.buyticket(3)
+person_b.checktoken()
