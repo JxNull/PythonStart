0de22cd9bc80d4f594c9ce7f90a5031240bbdf57
JxNull <35373708+JxNull@users.noreply.github.com>
2018-07-22 19:39:17 -0400

    Update Python_020_download_webpage.py

--- Python_Baisc/Python_020_download_webpage.py
+++ Python_Baisc/Python_020_download_webpage.py
@@ -6,14 +6,25 @@
 
 '''download a web page example'''
 import urllib.request
-import random
 
-file=urllib.request.urlopen('http://google.com')
+address_url=r'https://www.google.ca'
 
-data= file.read()               # read all page
+def download_webpage(url):
+    #open the url file
+    fileOpen=urllib.request.urlopen(url)
+    #read the file
+    data= fileOpen.read()
+    # convert into string
+    data_str = str(data)
+    # split the lines into the file
+    data_lines= data_str.split('\\n')
+    # creates a new file to save data
+    fhandle=open("download_wevpage.html","w")   # save web page in local
+    # store everything in created file
+    for line in  data_lines:
+        fhandle.write(line + '\n')
+    # close the file to save memory
+    fhandle.close()
 
-dataline=file.readline()        # read a line
-
-fhandle=open("./1/html","wb")   # save web page in local
-fhandle.write(data)
-fhandle.close()
+# run the code
+download_webpage(address_url)
